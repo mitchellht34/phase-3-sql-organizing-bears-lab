30fc228f8fb96379a14bf5a6f20e187e0b3802d3
Mitch Toran <mitchellht34@gmail.com>
2022-04-04 11:20:54 -0400

    commit tree 08129447011b18cde2605d859d75cdee1e6e1614

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 20, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 8, 'M', 'red', 'intense', 0);
+INSERT INTO bears VALUES (3, 'Tabitha', 19, 'F', 'blue', 'slimy', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 4, 'Mm', 'brown', 'nice', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 14, 'F', 'black', 'goofy', 0);
+INSERT INTO bears VALUES (6, 'Grinch', 3, 'M', 'green', 'grinchy', 0);
+INSERT INTO bears VALUES (7, 'Wendy', 18, 'F', 'red', 'naive', 1);
+INSERT INTO bears VALUES (8, NULL, 2, 'F', 'yellow', 'aggressive', 0);
